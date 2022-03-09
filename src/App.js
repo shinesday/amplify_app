08075ead19b6ae89d5902675d53f08dfc17b7a3e
Momo Jo <momo@momos-mbp.lan>
2022-03-08 21:52:05 -0800

    added auth

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from 'aws-amplify';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from '@aws-amplify/ui-react'
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
-
-function App() {
+function App({ signOut, user }) {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>We now have Auth!</h1>
+        <button onClick={signOut}>Sign out</button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
